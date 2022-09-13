8f9d95b4444c3539b1ac91f522e22659fd6a10fe
filipzlatojevic <ficozlatojevic@gmail.com>
2022-09-13 17:18:07 +0200

    add more style and js

--- appDropdown.js
+++ appDropdown.js
@@ -1,7 +1,14 @@
 var burger = document.querySelector('.burger');
 var navContainer = document.querySelector('.nav-container');
+var filterBurger = document.querySelector('.filter-burger');
+var filter = document.querySelector('.filter');
 
 burger.addEventListener('click', function () {
     navContainer.classList.toggle('active');
     burger.classList.toggle('active');
-})
+});
+
+filterBurger.addEventListener('click', function () {
+    filter.classList.toggle('active-filter');
+    filterBurger.classList.toggle('active');
+});
