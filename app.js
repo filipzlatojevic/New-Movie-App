8234c6c9933fafee569c500e1b694e810a5b4d00
filipzlatojevic <ficozlatojevic@gmail.com>
2022-09-14 16:28:30 +0200

    add seeMore button where you can see trailer video of movie

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 const form = document.querySelector('#searchForm');
 const container = document.querySelector('.container');
 
-const homeBtn = document.querySelector('.homeBtn');
+const homeBtn = document.querySelectorAll('.homeBtn');
 const popular = document.querySelector('#popular');
 const topRated = document.querySelector('#topRated');
 const upcoming = document.querySelector('#upcoming');
-const best = document.querySelector('#best');
 
-const links = document.querySelectorAll('.genres li');
+const links = document.querySelectorAll('.genres a');
 
 // Dates
 let currentDate = new Date().toJSON().slice(0, 10);
@@ -21,15 +20,15 @@ let futureDate = new Date(result).toJSON().slice(0, 10);
 const api_key = 'api_key=55c9fee551b2a928c4ee12a92f75b54a';
 const base_url = 'https://api.themoviedb.org/3';
 
-const popular_url = base_url + '/discover/movie?sort_by=popularity.desc&' + api_key;
+const popular_url = base_url + '/movie/popular?' + api_key;
 const upcoming_url = base_url +
     `/discover/movie?primary_release_date.gte=${currentDate}&primary_release_date.lte=${futureDate}&` + api_key;
 const top_rated_url = base_url +
     `/movie/top_rated?${api_key}&language=en-US&page=1`;
 const best_url = base_url +
     '/discover/movie?primary_release_year=2010&sort_by=vote_average.desc&' + api_key;
-
-const query = base_url + `/search/movie?${api_key}&query=`; // For searching movies
+// For searching movies
+const query = base_url + `/search/movie?${api_key}&query=`;
 
 // Home page
 const home = async function home() {
@@ -40,8 +39,8 @@ const home = async function home() {
 }
 window.onload = home;
 
-// Home button
-homeBtn.addEventListener('click', home);
+// Home buttons
+homeBtn.forEach(btn => btn.addEventListener('click', home));
 
 // Navbar:
 // Popular
@@ -73,11 +72,11 @@ upcoming.addEventListener('click', async function () {
 //     importMovies(res);
 // });
 
-// Genres
+// Sidebar Genres
 for (let li of links) {
     li.addEventListener('click', async function () {
         container.innerHTML = '';
-        const id = parseInt(this.value);
+        const id = parseInt(this.parentElement.value);
         const genres_url = base_url +
             `/discover/movie?with_genres=${id}&primary_release_year=2022&` + api_key;
         const res = await axios.get(genres_url);
@@ -100,10 +99,11 @@ form.addEventListener('submit', async function (e) {
 // Function for importing data from api, creating elements and appending them to document
 function importMovies(res) {
     for (let el of res.data.results) {
-        let path = el.poster_path;
-        let title = el.original_title;
-        let score = el.vote_average;
-        let overview = el.overview;
+        const { poster_path: path,
+            original_title: title,
+            vote_average: score,
+            overview,
+            id } = el;
 
         if (path) {
             const imgSource = 'https://image.tmdb.org/t/p/w500/' + path;
@@ -119,12 +119,53 @@ function importMovies(res) {
         <div class="overview">
             <div class="overview-info">
             <h3>Overview</h3>
-            <span class="seeMore">See more</span>
+            <span class="seeMore" id="${id}">See more</span>
         </div>
         ${overview}
     </div>
     `
             container.append(movieDiv);
+
+            document.getElementById(id).addEventListener('click', function () {
+                console.log(id);
+                openNav(el);
+            });
+
         }
     }
+}
+
+const overlayContent = document.querySelector('.overlay-content');
+/* Open when someone clicks on the span element */
+async function openNav(movie) {
+    let id = movie.id;
+    const res = await axios.get(base_url + '/movie/' + id + '/videos?' + api_key);
+    console.log(res.data.results);
+    if (res) {
+        document.getElementById("myNav").style.width = "100%";
+        if (res.data.results.length > 0) {
+            const videoList = [];
+            res.data.results.forEach(video => {
+                let { name, key, site } = video;
+                if (site == 'YouTube') {
+                    videoList.push(`
+                
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" 
+                    title="${name}" frameborder="0"
+                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen></iframe>
+                    
+                    `);
+                }
+            });
+            overlayContent.innerHTML = videoList.join('');
+        } else {
+            overlayContent.innerHTML = `<h1>No Results Found</h1>`;
+        }
+    }
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
 }
